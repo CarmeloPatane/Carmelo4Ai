0b32156537846c6f3412fe9f0c4e3147b1d951f3
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-09-15 09:54:20 +0000

    Modifica la classe Main per leggere da un file "test.txt" invece che dall'input della console

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
-public class Main(){
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main{
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        System.out.println(s.nextLine());
+        try{
+            File file = new File("test.txt");
+            Scanner s = new Scanner(file);
+            System.out.println(s.nextLine());
+            s.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        
     }
 }
