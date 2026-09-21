fd9f29ac9c3d0f1b35a5d76ffc30ed6bc21e0aef
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-09-21 09:04:27 +0000

    Modifica la classe Main per leggere da "file.txt", elaborare le righe e scrivere le parole in "output.txt". Aggiungi la classe Parola per gestire il contenuto delle parole.

--- Main.java
+++ Main.java
@@ -1,23 +1,39 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-public class Main{
-    public static void main(String[] args){
-        try{
-            File file = new File("testo.txt");
-            Scanner s = new Scanner(file);
-            while(s.hasNextLine()){
-                while(s.hasNext()){
-                    String word = s.next();
-                    System.out.println(word);
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            // Crea un riferimento al file da leggere.
+            File file = new File("file.txt");
+            File output = new File("output.txt");
+            // Apre il file per poterlo leggere con Scanner.
+            Scanner scanner = new Scanner(file);
+            PrintWriter writer = new PrintWriter(output);
+            String riga;
+            String[] parole;
+            String parola;
+            // Legge e stampa ogni riga fino alla fine del file.
+            while (scanner.hasNextLine()) {
+                riga = scanner.nextLine();
+                parole = riga.split("[ ;.,:?!']");
+                for (int i = 0; i < parole.length; i++) {
+                    parola = parole[i].toLowerCase().trim();
+                    if (!parola.equals("")&& parola.length()>3) {
+                        writer.println(parola);
+                    }
                 }
             }
-            s.close();
+            // Chiude lo Scanner e libera la risorsa associata al file.
+            scanner.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            // Mostra un messaggio se il file non esiste o non è accessibile.
+            System.out.println(e.getMessage());
         }
-        catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }
-        
     }
 }
---
+++ Parola.java
@@ -0,0 +1,7 @@
+public class Parola {
+    String contenuto;
+    int contatore = 0;
+    Parola(String contenuto){
+        this.contenuto = contenuto;
+    }
+}
