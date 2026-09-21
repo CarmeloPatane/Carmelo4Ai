9de415b9758c3f5d40e6adbc56b7fd56f74fd729
CarmeloPatane <carmelo.patane@marconirovereto.it>
2026-09-21 09:15:21 +0000

    Rinomina il campo 'contenuto' in 'testo' nella classe Parola e aggiorna i costruttori di conseguenza

--- Parola.java
+++ Parola.java
@@ -1,7 +1,18 @@
 public class Parola {
-    String contenuto;
-    int contatore = 0;
-    Parola(String contenuto){
-        this.contenuto = contenuto;
+    private String testo;
+    private int contatore;
+    public Parola(String testo, int contatore){
+        this.testo = testo;
+        this.contatore = contatore;
+    }
+    public Parola(String testo){
+        this(testo, 1);
+    }
+    public void incrementa(){
+        this.contatore ++;
+    }
+    @Override 
+    public String toString(){
+        return this.testo + ": " + this.contatore;
     }
 }
